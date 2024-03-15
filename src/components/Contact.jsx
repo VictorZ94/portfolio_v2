59fd5d4ff3524e5d600f86b351d69bd3b8036bc6
Victor Zuluaga <zrvictor@outlook.com>
2024-03-15 17:08:06 -0500

    created new section contact me

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <section className="max-w-screen-xl mx-auto p-4 mt-16 relative">
+      <h2 className="text-5xl my-4 dark:text-white text-center font-bold font-serif before:bg-gold-300 before:w-96 before:h-0.5 before:absolute before:left-0 before:top-2/4">
+        Contact me
+      </h2>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3"></div>
+    </section>
+  );
+};
+
+export default Contact;
